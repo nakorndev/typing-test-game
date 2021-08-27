6e7dd0f92bf5694a260715c12decba8b6b02f1dd
Nakorn Sinpadung <nakorndev@gmail.com>
2021-08-27 18:25:01 +0700

    เพิ่มสคริปสำหรับสลับฉาก

---
+++ game.js
@@ -0,0 +1,7 @@
+const scenePortal = document.getElementById('scenePortal')
+const sceneGame = document.getElementById('sceneGame')
+
+function gameStart () {
+    scenePortal.style.display = 'none'
+    sceneGame.style.display = 'block'
+}
