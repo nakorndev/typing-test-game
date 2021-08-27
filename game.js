b845456089328d58d6c3273b724193f2e3d29445
Nakorn Sinpadung <nakorndev@gmail.com>
2021-08-27 18:30:40 +0700

    เพิ่มวิธีการเก็บคะแนน

--- game.js
+++ game.js
@@ -1,7 +1,12 @@
 const scenePortal = document.getElementById('scenePortal')
 const sceneGame = document.getElementById('sceneGame')
+const gameScore = document.getElementById('gameScore')
 
 function gameStart () {
     scenePortal.style.display = 'none'
     sceneGame.style.display = 'block'
+}
+
+function increaseScore () {
+    gameScore.innerText = (Number(gameScore.innerText) + 1)
 }
